fde30e189eab7014203441b1b29ea1eac9fc23ea
gal levy <gallevy34@gmail.com>
2023-03-19 15:18:04 +0200

    delete some files

--- app/build/generated/source/navigation-args/debug/com/example/SecondHandGame/WelcomeFragmentDirections.java
+++ app/build/generated/source/navigation-args/debug/com/example/SecondHandGame/WelcomeFragmentDirections.java
@@ -1,16 +1,10 @@
 package com.example.SecondHandGame;
 
 import androidx.annotation.NonNull;
-import androidx.navigation.ActionOnlyNavDirections;
 import androidx.navigation.NavDirections;
 
 public class WelcomeFragmentDirections {
   private WelcomeFragmentDirections() {
-  }
-
-  @NonNull
-  public static NavDirections actionWelcomeFragmentToRecipesListFragment() {
-    return new ActionOnlyNavDirections(R.id.action_welcomeFragment_to_RecipesListFragment);
   }
 
   @NonNull
